f55c663efdd53e47ec42b08dd67d8b896fddfed3
mattshardman <mattshardman@gmail.com>
2019-01-28 12:24:11 +0000

    update main content text

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ const siteContent = {
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
-  "main-content": {
+  "mainContent": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4":"About",
@@ -37,28 +37,53 @@ const siteContent = {
   },
 };
 
+const { nav, cta, mainContent, contact, footer } = siteContent;
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 // update nav text and class names
 const navElements = document.querySelectorAll('a');
-const navClasses = Object.keys(siteContent.nav);
-const navText = Object.values(siteContent.nav);
-navElements.forEach((each, index) => { 
+const navClasses = Object.keys(nav);
+const navText = Object.values(nav);
+[...navElements].map((each, index) => { 
   each.className = navClasses[index],
   each.textContent = navText[index] 
 });
 
 // update call to action
 const { h1, button } = siteContent.cta;
-const cta = document.querySelector('.cta-text');
-const ctaHeader = cta.querySelector('h1');
-const ctaButton = cta.querySelector('button');
+const ctaSection = document.querySelector('.cta-text');
+const ctaHeader = ctaSection.querySelector('h1');
+const ctaButton = ctaSection.querySelector('button');
 const ctaImage = document.getElementById('cta-img');
 
 ctaHeader.textContent = h1;
 ctaButton.textContent = button;
 ctaImage.setAttribute('src', siteContent.cta['img-src']);
 
+//update main content section
+
+//update text-content
+const mainContentArray = Object.values(mainContent)
+  .filter((each, index) => index !== 4)
+
+const parasContent = mainContentArray
+  .filter((each, index) => index % 2 !== 0);
+
+const h4Content = mainContentArray
+  .filter((each, index) => index % 2 === 0);
+
+const textContent = document.querySelectorAll('.text-content');
+
+const h4s = document.querySelectorAll('h4');
+const paras = document.querySelectorAll('p');
+
+[...h4s].map((h4, index) => {
+  h4.textContent = h4Content[index];
+});
+
+[...paras].map((para, index) => {
+  para.textContent = parasContent[index];
+});
 
