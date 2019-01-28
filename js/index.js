cec469c6128a00bc6d0d8936b86869a13055cd1c
mattshardman <mattshardman@gmail.com>
2019-01-28 15:00:52 +0000

    completed timer stretch

--- js/index.js
+++ js/index.js
@@ -1,56 +1,79 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
+  nav: {
+    'nav-item-1': 'Services',
+    'nav-item-2': 'Product',
+    'nav-item-3': 'Vision',
+    'nav-item-4': 'Features',
+    'nav-item-5': 'About',
+    'nav-item-6': 'Contact',
+    'img-src': 'img/logo.png',
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
+  cta: {
+    h1: 'DOM Is Awesome',
+    button: 'Get Started',
+    'img-src': 'img/header-img.png',
   },
-  "mainContent": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  mainContent: {
+    'features-h4': 'Features',
+    'features-content': 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'about-h4': 'About',
+    'about-content': 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'middle-img-src': 'img/mid-page-accent.jpg',
+    'services-h4': 'Services',
+    'services-content': 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'product-h4': 'Product',
+    'product-content': 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'vision-h4': 'Vision',
+    'vision-content': 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    'contact-h4': 'Contact',
+    address: '123 Way 456 Street Somewhere, USA',
+    phone: '1 (888) 888-8888',
+    email: 'sales@greatidea.io',
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+  footer: {
+    copyright: 'Copyright Great Idea! 2018',
   },
 };
 
-const { nav, cta, mainContent, contact, footer } = siteContent;
+const {
+  nav, cta, mainContent, contact, footer,
+} = siteContent;
+
+function addChild(addTo, elementType, text, prepend) {
+  const child = document.createElement(elementType);
+  const t = document.createTextNode(text);
+  child.appendChild(t);
+  if (prepend) {
+    return addTo.prepend(child);
+  }
+  return addTo.appendChild(child);
+}
+
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"]);
+const logo = document.getElementById('logo-img');
+logo.setAttribute('src', siteContent.nav['img-src']);
 
 // UPDATE NAV TEXT AND CLASSNAMES
-const navElements = document.querySelectorAll('a');
+const navigation = document.querySelector('nav');
+const navElements = navigation.querySelectorAll('a');
 const navClasses = Object.keys(nav);
 const navText = Object.values(nav);
-[...navElements].map((each, index) => { 
+
+[...navElements].map((each, index) => {
   each.className = navClasses[index];
   each.textContent = navText[index];
+});
+
+addChild(navigation, 'a', 'Party');
+addChild(navigation, 'a', 'Fish', true);
+
+const newElements = navigation.querySelectorAll('a');
+[...newElements].map((each, index) => {
   each.style.color = 'green';
 });
+
 
 // UPDATE CALL TO ACTION
 const { h1, button } = siteContent.cta;
@@ -63,13 +86,13 @@ ctaHeader.textContent = h1;
 ctaButton.textContent = button;
 ctaImage.setAttribute('src', siteContent.cta['img-src']);
 
-//UPDATE MAIN CONTENT SECTION
+// UPDATE MAIN CONTENT SECTION
 
-//update text-content
+// update text-content
 
-//filter main content for h4s and paras
+// filter main content for h4s and paras
 const mainContentArray = Object.values(mainContent)
-  .filter((each, index) => index !== 4)
+  .filter((each, index) => index !== 4);
 
 const parasContent = mainContentArray
   .filter((each, index) => index % 2 !== 0);
@@ -77,12 +100,12 @@ const parasContent = mainContentArray
 const h4Content = mainContentArray
   .filter((each, index) => index % 2 === 0);
 
-//select h4s and paras from DOM
+// select h4s and paras from DOM
 const mainContentSection = document.querySelector('.main-content');
 const h4s = mainContentSection.querySelectorAll('h4');
 const paras = mainContentSection.querySelectorAll('p');
 
-//set h4s and paras from JSON
+// set h4s and paras from JSON
 [...h4s].map((h4, index) => {
   h4.textContent = h4Content[index];
 });
